4211fc1ec9c2e5383a27b4b8622a00395f7bbfba
antony_sanders <antony.devon92@gmail.com>
2020-08-18 01:24:25 -0400

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,56 @@
-let state;
+function createStore() {
+    let state;
+
+    function dispatch(action) {
+        state = reducer(state, action);
+        render();
+    };
+
+    function getState() {
+        return state;
+    }
+
+    return { dispatch, getState };
+}
 
 function reducer(state = { count: 0 }, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
+    switch (action.type) {
+        case 'INCREASE_COUNT':
+            return { count: state.count + 1 };
 
-    default:
-      return state;
-  }
+        case 'DECREASE_COUNT':
+            return { count: state.count - 1 };
+
+        case 'RESET_COUNT':
+            return { count: state.count = 0 };
+
+        default:
+            return state;
+    }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
 
 function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
+    let container = document.getElementById('container');
+    container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
+let store = createStore(reducer)
+store.dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+    setInterval(increaseByOne, 1000);
+})
+
+function increaseByOne() {
+    store.dispatch({ type: 'INCREASE_COUNT' })
+}
+
+button2.addEventListener('click', function() {
+    store.dispatch({ type: 'DECREASE_COUNT' });
+})
+
+button3.addEventListener('click', function() {
+    store.dispatch({ type: 'RESET_COUNT' });
 })
